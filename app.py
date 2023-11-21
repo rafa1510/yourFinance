54442fdb55e883385abc98af5a5f54dd055ca527
rafa1510 <rcestti78@gmail.com>
2023-11-21 02:40:42 -0500

    Initialize backend for login

--- app.py
+++ app.py
@@ -99,21 +99,25 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("Must provide username")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("Must provide password")
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = db.session.execute(db.select(User).where(User.username == username)).scalar()
+        if user == None:
+            return apology("User does not exist")
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+        if not check_password_hash(user.hash, password):
+            return apology("Invalid password")
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = user.id
 
         # Redirect user to home page
         return redirect("/")
